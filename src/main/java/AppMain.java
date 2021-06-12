7aaf79cea8defa9d3d0b2265ceeddecd27163519
Carlos <carlos.av.bit@gmail.com>
2021-06-12 14:52:30 -0600

    Program made for testing before implementing in the real system

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,18 +1,27 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class AppMain {
     public static void main(String[] args) {
+
         ArrayList<Aircraft> aircraftList = new ArrayList<>();
         ArrayList<Flight> flightData = new ArrayList<>();
 
-        InitialData loadInformation = new InitialData(aircraftList,flightData);
+        InitialData loadInformation = new InitialData();
+        loadInformation.returnData(aircraftList,flightData);
 
+        //MAIN MENU
+        Menu start = new Menu();
+        start.mainMenu();
+
+
+
+        /*
         ExcelGenerator generate = new ExcelGenerator("FlightList.xlsx", "flights");
+
         generate.generate(flightData);
 
         GeneratePDF flightPDF = new GeneratePDF();
         flightPDF.FlightPDF(flightData);
-
+        */
     }
 }
